8aa9736befc80205e8935240a8267b7d59ff5894
daoviedo26 <joviedo80420@ufide.ac.cr>
2024-08-17 20:51:30 -0600

    Se agrega código para procesamiento de imagenes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Artvisionary-AI
 Artvisionary-AI es una aplicación impulsada por inteligencia artificial que identifica y ofrece información detallada sobre obras de arte, monumentos y otros objetos culturales a partir de imágenes. Además, sugiere lugares de interés cercanos, conectando a los usuarios con la historia y cultura a su alrededor.
+
+
---
+++ backend/app/services/image_processing.py
@@ -0,0 +1,15 @@
+from PIL import Image
+from transformers import BlipProcessor, BlipForConditionalGeneration
+
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large").to("cuda")
+
+IMAGE_PATH = 'img3.jpg'
+raw_image = Image.open(IMAGE_PATH).convert('RGB')
+
+# conditional image captioning
+text = "a photography of"
+inputs = processor(raw_image, text, return_tensors="pt").to("cuda")
+
+out = model.generate(**inputs)
+print(processor.decode(out[0], skip_special_tokens=True))
